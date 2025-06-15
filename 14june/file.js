6274c1ac27efdb9e88b8a19aa7316178db1c3deb
nishant thakur <nishantt7049@gmail.com>
2025-06-15 22:28:54 +0530

    message

---
+++ 14june/file.js
@@ -0,0 +1,76 @@
+// variabkes
+
+// var let const
+
+// var a = 12
+// let b = "nusgbat"
+// const c = 57.0
+
+
+// with var we can redeclare the same variables and also reasign the value and var has global scope
+
+// var num = 10
+// var num = 12
+
+// num is declared and the value is assigned
+
+// in let we cant do the redeclare but can do reassign and local scope
+// let n = 3
+// n = 13
+
+
+//const name 
+//name =" "
+// cant reassign and redeclare 
+
+// speciala type of symbols are called operators
+// of three types 
+
+// uniray 
+// ++,--
+
+
+
+// binary
+// arthmatic 
+// +,-,*,/,%
+
+// assignment
+// =,+=,-=,*=,/=
+
+// ralational
+// <,>,<=,>=,==,!=
+
+// logical
+// &&,||,!
+
+
+// turnary  ---- conditinal operator
+// ?,:
+
+
+
+// write a program to find area of circle 
+// r =4
+// and print it on console
+
+
+
+// var r = 4
+// console.log(3.14*r*r)
+
+
+
+
+// var a = parseInt (prompt("enter a value"))
+// var b =  parseInt (prompt("enter another value"))
+
+// console.log(a+b)
+
+
+var a = prompt("enter a value")
+var b = prompt("enter another value")
+
+console.log(a+b)
+
+
---
+++ file.js
@@ -0,0 +1,8 @@
+console.log("hi from external file")
+document.write("its from external")
+
+//
+// variable
+//if we have to declare variable in js
+// var,let,const and these are keywords
+// its not start with num.specialchar.cantgivespacebtw.
