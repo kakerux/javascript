b724cc71087c66298dacede97e5bbef8fef7d9b7
nishant thakur <nishantt7049@gmail.com>
2025-06-17 20:41:00 +0530

    if else

--- 14june/pp.js
+++ 14june/pp.js
@@ -1,5 +1,5 @@
 // var r = 4
-// console.log(r*r)
+// document.write(r*r)
 
 
 // let r = 5
@@ -15,29 +15,58 @@
 
 
 
-var a = parseInt(prompt("enter a value"))
-var b = parseInt(prompt("enter 2nd value"))
-var operator = prompt("select an operator = + , - , * , /")
-var result
-if(operator == '+'){
-    result = a+b 
-}
-else if(operator == '-'){
-    result = a-b
-}
-else if(operator == '*'){
-    result = a*b
-}
-else if(operator == '/'){
-    if(b!==0){
-        result = a/b
-    }
-    else{
-        result = "invalid"
-    }
-}
-else{
-    result = "its invalid "
-}
-console.log("result :",result)
+// var a = parseInt(prompt("enter a value"))
+// var b = parseInt(prompt("enter 2nd value"))
+// var operator = prompt("select an operator = + , - , * , /")
+// var result
+// if(operator == '+'){
+//     result = a+b 
+// }
+// else if(operator == '-'){
+//     result = a-b
+// }
+// else if(operator == '*'){
+//     result = a*b
+// }
+// else if(operator == '/'){
+//     if(b!==0){
+//         result = a/b
+//     }
+//     else{
+//         result = "invalid"
+//     }
+// }
+// else{
+//     result = "its invalid "
+// }
+// console.log("result :",result)
+
+
+
+// SHOWS IN CONSOLE
+// var a = prompt("Enter the number : ")
+// if(a>0){
+//     console.log("positive")
+// }
+// else if(a<0){
+//     console.log("negative")
+// }
+// else if(a==0){
+//     console.log("zero")
+// }
+// else{console.log("invalid")}
+
+
+// SHOWS IN THE PAGE OR Document
+// var a = prompt("Enter the number : ")
+// if(a>0){
+//     document.write("positive")
+// }
+// else if(a<0){
+//     document.write("negative")
+// }
+// else if(a==0){
+//    document.write("zero")
+// }
+// else{document.write("invalid")}
 
