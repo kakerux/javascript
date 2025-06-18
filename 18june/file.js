81fba6844e1ff2ec842dad519c8609a122e9f371
nishant thakur <nishantt7049@gmail.com>
2025-06-18 23:54:51 +0530

    loop

---
+++ 18june/file.js
@@ -0,0 +1,58 @@
+// loops
+// while 
+// for 
+// do while
+// for of 
+// for in
+// forEach-
+
+// var n = 1
+// do{
+
+//     console.log(n)
+//     n++
+// }
+// while(n<11)
+
+
+
+// write a program to print a table of any No .
+
+// var i = 1
+// var a = parseInt(prompt("enter a number"))
+// do{
+//     console.log(a*i)
+//     i++
+// }
+// while(i<11)
+
+//     write a program to print ms 15 times
+// var a = "Microsoft"
+// var b = 1
+// while(b<16){
+// //   console.log(a)
+//   document.write(a,"<br>")
+//   b++
+// }
+
+// write a program to print a reverse table of any Node. using while loop
+// var a = parseInt(prompt("enter the number"))
+// var b = 10
+// while(b>0){
+//     console.log(a*b)
+//     b--
+// }
+
+
+// write a program to print only odd numbers between 20 to 1
+// var b = 20
+// while(b>0){
+//    if(b%2!==0) {
+//     console.log(b)}
+//     b--
+// }
+
+
+
+
+   
