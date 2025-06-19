cd64b67a511b85a4019e3d1ba283a08f50500fab
nishant thakur <nishantt7049@gmail.com>
2025-06-19 23:23:54 +0530

    for loop

---
+++ 19june/file.js
@@ -0,0 +1,49 @@
+// for(initialization;condition;inc/dec){}
+
+// for(var n=1 ; n<=10 ; n++){
+//     console.log(n)
+// }
+
+
+
+// user input interger value - 
+
+// var n = parseInt(prompt("enter the number"))
+// if(n>=1 && n<=10){
+//     for(var a=1 ; a<=n ; a++){
+//         console.log(a)
+//     }
+// }
+// else if(n>=11 && n<=20){
+//     for(var a = 20 ; a>=1 ; a--){
+//         console.log(a)
+//     }
+// }
+// else{console.log(invalid)}
+
+
+// write a program to print square of all even numbers between 20 to 1
+// for(var n = 20 ; n>1 ; n--){
+//     if(n%2==0){
+//         console.log(n*n)
+//     }
+// }
+
+
+// for(var n=1 ; n<=3 ; n++){
+//     for(var a =1 ; a<=10 ; a++){
+//         console.log(a*n)
+//     }
+// }
+
+
+// for(var n = 1 ; n<=5 ; n++){
+//     document.write('*')
+// }
+
+for(var n = 1 ; n<=5 ; n++){
+    for(var a=1 ; a<=n ; a++){
+       document.write('*')
+
+    }  
+}
