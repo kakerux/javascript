fb7857a39b300a7eadff18d2d486cf685190c471
nishant thakur <nishantt7049@gmail.com>
2025-06-21 13:13:55 +0530

    array

---
+++ 20june/fiel.js
@@ -0,0 +1,60 @@
+// for(let r=1 ; r<=5 ; r++){
+//     for(let c=1 ; c<=5 ; c++){
+//         if(r==1 || r==5 || c==1 || c==5){
+//             document.write("*")
+//         }
+//         else{
+//             document.write("&nbsp&nbsp")
+//         }
+//     }
+//     document.write("<br>")
+// }
+
+
+// collection of multiple data - array
+// []
+// ordered -  because of -indexing - -- position of element
+// indexing - start from 0
+
+
+// let ar = ["aman",34,4.5,'B',true]
+// console.log(ar)
+// console.log(ar[3])
+// for(let i=0 ; i<5 ; i++){
+//     console.log(ar[i])
+// }
+
+
+
+// ARRAY 
+
+// let ar = [1,2,3,4,5,6,7,8,9,10]
+// for(let i=0 ; i<=9 ; i++){
+//     console.log(ar[i])
+// }
+
+// for(let i=0 ; i<=9 ;i++){
+//     if(i%2==0){
+//         console.log(ar[i])
+//     }
+// }
+
+// for(let i=0 ; i<=9 ;i++){
+//     if(ar[i]%2==0){
+//         console.log(ar[i])
+//     }
+// }
+
+
+
+// why indexing starts from 0
+// let ar = [1,2,3,4,5,6,7,8,9,10]
+// for of store the value of array and print them
+// for(let vl of ar){
+//     console.log(vl)
+// }
+
+// for in store index of array in variable and print them 
+// for(let vt in ar){
+//     console.log(vt)
+// }
