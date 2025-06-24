61d655d9de8ec7511c0ed3721b97d2881f660d8c
nishant thakur <nishantt7049@gmail.com>
2025-06-24 21:15:02 +0530

    object

---
+++ 24june/file.js
@@ -0,0 +1,60 @@
+// OBJECTS
+// --DATA TYPE--STORE MULTIPLE TYOE OF DATA
+// {}
+// NOT ORDERED -- NO INDEXING
+// KEY : VALUE ---STORE HOTI HAIN
+// let ob = {
+//     name : "nishant",
+//     contact : 1234,
+//     city : 'bhopal' 
+// }
+// console.log(ob)
+// console.log(ob.name)  -- PRINTS VALUE OF THE KEY
+
+
+// let a = prompt("name")
+// let b = prompt("city")
+// let c = prompt("contact")
+// let ob = {
+//     name :prompt("name") ,
+//     city : prompt("city"),
+//     contact :parseInt(prompt("contact"))
+// }
+// console.log(ob.name)
+
+
+// let emp = {
+//     emp_name : 'nishant',
+//     emp_contact : [343434,342232423],
+//     emp_address : ['bpl','mp'],
+//     emp_id:21,
+// }
+// console.log(emp.emp_contact[1]) --- PRINTS THE VALUE OF ARRAY AT INDEX 1
+// console.log(emp.emp_address[1])
+
+
+// let lang = {
+//         name:'qasim',
+//         emp_language:['java','python','html']
+// }
+// lang.emp_language.push('cpp')
+// console.log(lang)
+
+// IF YOU WANT TO ADD THEN ----OBJECT.KEY=VALUE
+// lang.name='nishant'
+// THIS WILL ALSO OVERWRITE THE VALUE IF YOUR KEY IS SAME
+// console.log(lang)
+
+
+// IF YOU WANT TO FIND OUT KEYS USE FOR IN LOOP
+// for(let k in lang){
+    // console.log(k)
+// console.log(k)
+
+    // IT WILL PRINT VALUES OF OBJECT
+//     console.log(lang[k])
+// }
+
+
+
+// PRINT ONLY ONE KEY FROM OBJECT
