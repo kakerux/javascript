1bfbffb25e3a9bda10ed95e22fb3f7e296abb6ea
nishant thakur <nishantt7049@gmail.com>
2025-06-25 21:24:04 +0530

    function

---
+++ 25june/file.js
@@ -0,0 +1,69 @@
+// function -- block of instructions is function--use it for reusibility
+
+// function identifire (){
+    // code 
+// }
+
+// 3 -> function declaration
+//   -> function defination
+// ->  function calling
+
+function add(){
+    console.log('inside the function')
+}
+// if you dont call the function like given below then it will not use it 
+add() 
+
+// write a progam write a print a table of any no.
+// function table(){
+//     let a = parseInt(prompt("enter a number"))
+//     for(let n = 1 ; n<=10 ; n++){
+//         console.log(a*n)
+//     }
+// }
+// table()
+
+
+// abhi ke liye 2 types ke function user define--- which have to define then call
+//  , predefine--which we have to call only
+// user define is of 4 types
+//1 -> without aurrgument and no return value
+// 2 -> with aurgument and no return value
+// 3-> with aurgument and with return value
+// 4-> without aurgument and with return value
+
+// argument work as a input in function
+// ex. table(8) -- 8 is argument
+// function table("its a parameter") --
+
+
+
+// function type1(a){
+//     return a+a; -- return value return at calling -type(b)
+// }
+// let b = 8
+// type(b) --- ( b = a) 
+// let c = type1(b) -- if you want to perform function using that return value than 
+// store it in a variable then 
+// or
+// c.log(type1(b)) -- if you want to print the return value than simply c.log 
+// the calling
+
+
+// function cloud(a){
+//     console.log(a*a*a)
+// }
+// cloud(8)
+
+// write a program to check no. is positive or negative using function type 2
+// function no(a){
+// if(a>0){
+//     console.log('positive')
+// }
+// else if(a<0){
+//     console.log('negative')
+// }
+// else{console.log('zero')}
+// }
+// no(parseInt(prompt('enter value')))
+
