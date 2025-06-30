21c9889af00413e0beaff692ad27ef13ac01e849
nishant thakur <nishantt7049@gmail.com>
2025-06-30 23:14:06 +0530

    onclick related

---
+++ 30june/file.js
@@ -0,0 +1,11 @@
+function fun(){
+    alert('working....')
+}
+
+
+    // let head = document.getElementById('head')  you can do both
+function changetext(){
+    let head = document.getElementById('head')
+    head.textContent = 'yes you are right'
+}
+
---
+++ 30june/task.js
@@ -0,0 +1,38 @@
+//  let head = document.getElementById('head')
+
+// function fun(){
+//     // let head = document.getElementById('head')
+//     head.textContent = "Get Lost"
+//     head.style.color = 'red'
+// }
+// function task(){
+//     head.textContent = 'Hellow'
+//     head.style.color = 'black'
+
+// }
+
+// the value comes in 'a' as attribute and while the function is same for all 
+// and runs for all function values cuz task is same 
+// function same chalega value alag hai to alag kaam karega
+let head1 = document.getElementById('head1')
+// function red(a){
+//     document.body.style.backgroundColor=a
+//     head1.textContent=a
+// }
+// function green(){
+//     document.body.style.backgroundColor="green"
+//     head1.textContent='Green'
+// }
+// function yellow(){
+//     document.body.style.backgroundColor="yellow"
+//     head1.textContent='Yellow'
+// }
+
+function color(a){
+    document.body.style.backgroundColor=a
+    head1.textContent=a
+}
+
+
+// make a counter 
+// 0 -> -,+   click on plus it plus and vice versa
